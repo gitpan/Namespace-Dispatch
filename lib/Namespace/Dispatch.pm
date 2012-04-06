161845ed05407e0a9971fcc011528dfcce87bbe1
許家瑋 <shelling@cpan.org>
2012-04-06 16:35:19 +0000

    Import of SHELLING/Namespace-Dispatch-0.02 from CPAN.
    gitpan-cpan-distribution: Namespace-Dispatch
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         SHELLING/Namespace-Dispatch-0.02.tar.gz
    gitpan-cpan-author:       SHELLING
    gitpan-cpan-maturity:     released

--- lib/Namespace/Dispatch.pm
+++ lib/Namespace/Dispatch.pm
@@ -1,5 +1,5 @@
 package Namespace::Dispatch;
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 use 5.010;
 use UNIVERSAL::filename;
@@ -62,30 +62,52 @@ Namespace::Dispatch - A dispatcher treating namespaces as a tree
 
 =head1 SYNOPSIS
 
+    # lib/Foo.pm
     package Foo;
     use Namespace::Dispatch;
 
+    1;
+
+    # lib/Foo/Bar.pm
     package Foo::Bar;
     use Namespace::Dispatch;
 
+    1;
+
+    # lib/Foo/Bar/Baz.pm
     package Foo::Bar::Baz;
     use Namespace::Dispatch;
 
+    1;
+
+    # lib/Foo/Bar/Baz/Next.pm
+    package Foo::Bar::Baz::Next;
+    use Namespace::Dispatch;
+
+    1;
+
+    # any.pl
     package main;
     use Foo;
-    Foo->dispatch(qw(bar baz)); #=> Foo::Bar::Baz
+    Foo->dispatch(qw(bar baz));            #=> Foo::Bar::Baz
+    Foo->dispatch(qw(bar baz next));       #=> Foo::Bar::Baz::Next
+    Foo::Bar->dispatch(qw(bar baz next));  #=> Foo::Bar::Baz::Next
+    Foo->dispatch(qw(hello world));        #=> Foo
 
 =head1 DESCRIPTION
 
 Namespace::Dispatch is designed for the purpose that tasks are broke into a set of relevant, hierarchical modules.
-Implicit dispatching ability was attached into these modules when they are declared as members of this set.
-Any node in this hierarchy can serve the dispatching request in recursive manner.
+Implicit dispatching ability was attached into these modules when they are declared as members of this set.  Any node in
+this hierarchy can serve the dispatching requests in recursive manner. That is, Any tree-like routing system can adopt
+the abstraction under the hood with its own invoking mechanism.
 
 =head1 AUTHOR
 
 shelling E<lt>navyblueshellingford@gmail.comE<gt>
 
 =head1 SEE ALSO
+
+L<App::LDAP>
 
 =head1 LICENSE
 
